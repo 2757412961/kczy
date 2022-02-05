058f1cebe8926ab2cccd5e6351214fc8cffbdf71
Wang Luoqi <1446735020@qq.com>
2022-02-05 22:39:39 +0800

    [Append]Phase1 finished.

--- src/main/java/org/dde/zju/kczy/KCZYYC.java
+++ src/main/java/org/dde/zju/kczy/KCZYYC.java
@@ -20,11 +20,15 @@ import java.util.stream.Collectors;
 public class KCZYYC {
 
     private static final Logger logger = LoggerFactory.getLogger(KCZYYC.class);
-    private static final String SSH_IP = "127.0.0.1";
+//    private static final String SSH_IP = "127.0.0.1";
+    private static final String SSH_IP = "120.27.216.174";
     private static final int SSH_PORT = 22;
-    private static final String SSH_USERNAME = "hulinshu";
-    private static final String SSH_PASSWORD = "hu";
-    private static final String SSH_TEMP_FILE_DIR = "/home/hulinshu/data/temp/";
+    private static final String SSH_USERNAME = "root";
+    private static final String SSH_PASSWORD = "upxTP1oR5SIbeQKG";
+    private static final String SSH_REMOTE_FILE_DIR = "/home/hulinshu/data/aster/"; //data in ssh computer(just for test)
+    private static final String SSH_TEMP_VNIR_FILE_DIR = "/home/hulinshu/data/temp/vnir/";
+    private static final String SSH_TEMP_SWIR_FILE_DIR = "/home/hulinshu/data/temp/swir/";
+    private static final String SSH_TEMP_ARGILLIC_FILE_DIR = "/home/hulinshu/data/temp/argillic/";
     private static final String PROCESS_TOOLSET_DIR = "/home/hulinshu/tool/extractband_shibian/";
     private static final String FRAME_TOOLSET_DIR = "/home/hulinshu/tool/fengfu_tiantu/";
 
@@ -39,7 +43,7 @@ public class KCZYYC {
                 .getOrCreate();
         JavaSparkContext jsc = new JavaSparkContext(ss.sparkContext());
 
-        String inputDir = args[0];
+        String inputDir = args[0]; //local data dir
         File fileDir = new File(inputDir);
         if (fileDir.isFile()) {
             logger.error("Input should be a directory, exit");
@@ -55,7 +59,7 @@ public class KCZYYC {
         JavaRDD<String> filePathsRDD = jsc.parallelize(filePaths);
         filePathsRDD.cache();
 
-        // Phrase 1: Alteration
+        // Phase 1: Alteration
         // TODO data preparation (Extract band -> Crosstalk -> Radiometric calibration && Atmospheric correction)
         // step 1.1: export vnir
         JavaRDD<String> vnirFilePathsRDD = filePathsRDD.map(new Function<String, String>() {
@@ -64,12 +68,13 @@ public class KCZYYC {
                 // TODO export vnir shell
                 SecureShell ss = new SecureShell(SSH_IP, SSH_USERNAME, SSH_PASSWORD, SSH_PORT);
                 String outputFileName = s.replace(".hdf", ".tif");
-                String outputFilePath = SSH_TEMP_FILE_DIR + "vnir/" + outputFileName;
-                String sshTempFile = SSH_TEMP_FILE_DIR + "vnir/" + s + ".sh";
+                String outputFilePath = SSH_TEMP_VNIR_FILE_DIR + outputFileName;
+                String sshTempFile = SSH_TEMP_VNIR_FILE_DIR + s + ".sh";
                 // prepare cmd
                 StringBuilder sb = new StringBuilder("cd " + PROCESS_TOOLSET_DIR + "; wine ");
                 sb.append("ExportVNIR.exe ");
-                sb.append(inputDir + File.separator + s + " ");
+                sb.append(SSH_REMOTE_FILE_DIR + s + " "); // 在manager为windows系统下使用
+//                sb.append(inputDir + File.separator + s + " ");
                 sb.append(outputFilePath);
                 String result = ss.runWithOutput(sb.toString(), sshTempFile);
                 logger.info(result);
@@ -81,32 +86,61 @@ public class KCZYYC {
         JavaRDD<String> swirFilePathsRDD = filePathsRDD.map(new Function<String, String>() {
             @Override
             public String call(String s) throws Exception {
-                // TODO export swir shell
+                // export swir shell
                 SecureShell ss = new SecureShell(SSH_IP, SSH_USERNAME, SSH_PASSWORD, SSH_PORT);
                 String outputFileName = s.replace(".hdf", ".tif");
-                String outputFilePath = SSH_TEMP_FILE_DIR + "swir/" + outputFileName;
-                String sshTempFile = SSH_TEMP_FILE_DIR + "swir/" + s + ".sh";
+                String outputFilePath = SSH_TEMP_SWIR_FILE_DIR + outputFileName;
+                String sshTempFile = SSH_TEMP_SWIR_FILE_DIR + s + ".sh";
                 // prepare cmd
                 StringBuilder sb = new StringBuilder("cd " + PROCESS_TOOLSET_DIR + "; wine ");
                 sb.append("ExportSWIR.exe ");
-                sb.append(inputDir + File.separator + s + " ");
+                sb.append(SSH_REMOTE_FILE_DIR + s + " ");
+//                sb.append(inputDir + File.separator + s + " ");
                 sb.append(outputFilePath);
                 String result = ss.runWithOutput(sb.toString(), sshTempFile);
                 logger.info(result);
                 return outputFilePath;
             }
         });
+        // step 1.3: Alteration
+        JavaRDD<String> argillicFilePathsRDD = filePathsRDD.map(new Function<String, String>() {
+            @Override
+            public String call(String s) throws Exception {
+                // export swir shell
+                SecureShell ss = new SecureShell(SSH_IP, SSH_USERNAME, SSH_PASSWORD, SSH_PORT);
+                String outputFileName = s.replace(".hdf", ".tif");
+                String outputFilePath = SSH_TEMP_ARGILLIC_FILE_DIR + outputFileName;
+                String sshTempFile = SSH_TEMP_ARGILLIC_FILE_DIR + s + ".sh";
+                // prepare cmd
+                StringBuilder sb = new StringBuilder("cd " + PROCESS_TOOLSET_DIR + "; wine ");
+                // function name
+                sb.append("GetArgillic.exe ");
+                // vnir filename
+                sb.append(SSH_TEMP_VNIR_FILE_DIR + outputFileName + " ");
+                // swir filename
+                sb.append(SSH_TEMP_SWIR_FILE_DIR + outputFileName + " ");
+                // output argillic filename
+                sb.append(outputFilePath);
 
-        // TODO band data pre-processing
+                String result = ss.runWithOutput(sb.toString(), sshTempFile);
+                logger.info(result);
+                return outputFilePath;
+            }
+        });
 
-        // TODO Alteration
 
-        // Phrase 2: raming and mapping
+        // Phase 2: raming and mapping
         // TODO generate empty frames
+        // calculate boundary
+
+
 
         // TODO fill-in the frames
+
+        // action
         vnirFilePathsRDD.collect().forEach(logger::info);
         swirFilePathsRDD.collect().forEach(logger::info);
+        argillicFilePathsRDD.collect().forEach(logger::info);
 
         ss.stop();
         ss.close();
